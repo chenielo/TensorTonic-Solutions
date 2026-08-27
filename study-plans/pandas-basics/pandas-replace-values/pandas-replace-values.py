d0222d53bc80d102bebe9c51c26998f2e2dbad17
chenielo <chen.chocolateyo@gmail.com>
2026-08-27 16:03:01 -0400

    Solve pandas-replace-values

--- study-plans/pandas-basics/pandas-replace-values/pandas-replace-values.py
+++ study-plans/pandas-basics/pandas-replace-values/pandas-replace-values.py
@@ -6,7 +6,7 @@ def replace_values(data, column, old_val, new_val):
     """
     df = pd.DataFrame(data)
     cnt = (df[column] == old_val).sum()
-    df[column] = df[column].apply(lambda x: new_val if x == old_val else x)
+    df[column] = df[column].replace(old_val, new_val)
     return {
         "data":df.to_dict("list"),
         "count": cnt

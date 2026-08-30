bf998c92283c6bdf77341a865e36268f961c0cdd
chenielo <chen.chocolateyo@gmail.com>
2026-08-30 18:33:26 -0400

    Solve pandas-cross-tabulation

---
+++ study-plans/pandas-basics/pandas-cross-tabulation/pandas-cross-tabulation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def cross_tab(data, row_col, col_col):
+    """
+    Returns: nested dict {col_value: {row_value: frequency}}
+    """
+    df = pd.DataFrame(data) 
+    out = pd.crosstab(df[row_col], df[col_col])
+    return out.to_dict()

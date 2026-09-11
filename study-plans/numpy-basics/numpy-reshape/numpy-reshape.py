61e5b7b60aa81279e4763775495be4cdbd32507f
chenielo <chen.chocolateyo@gmail.com>
2026-09-11 11:18:04 -0400

    Solve numpy-reshape

---
+++ study-plans/numpy-basics/numpy-reshape/numpy-reshape.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def reshape_array(data, operation):
+    """
+    Returns: ndarray of float64 with shape determined by the operation
+    """
+    arr = np.asarray(data, dtype=np.float64)
+    if operation == "flatten":
+        return arr.flatten()
+    elif operation == "transpose":
+        return arr.T
+    return np.expand_dims(arr, axis=0)

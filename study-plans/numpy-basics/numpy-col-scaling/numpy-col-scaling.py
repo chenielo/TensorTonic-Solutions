ec61bf442b3054783dbb37e6de51af686ef2c2f0
chenielo <chen.chocolateyo@gmail.com>
2026-09-19 16:05:06 -0400

    Solve numpy-col-scaling

---
+++ study-plans/numpy-basics/numpy-col-scaling/numpy-col-scaling.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def scale_cols(data, weights):
+    """Returns: np.ndarray of shape (m, n), each column scaled by corresponding weight"""
+    arr = np.array(data, dtype=np.float64)
+    w = np.array(weights, dtype=np.float64)
+    return arr*w
